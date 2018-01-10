7e04329fa2cee539da9601801ff3bcb7b2668f22
tianxin <tianxin@163.com>
2018-01-10 16:05:54 +0800

    python文件移动一个文件夹

---
+++ python/python文件/NumMax.py
@@ -0,0 +1,6 @@
+num1=int(input("请输入一个数整数："))
+num2=int(input("请再次输入一个整数："))
+if num1 >=num2:
+    print("num1和num2从小到大的顺序是：",'num2:',num2,'num1:',num1)
+else :
+    print("num1和num2从小到大的顺序是：",'num1:',num1,'num2:',num2)
